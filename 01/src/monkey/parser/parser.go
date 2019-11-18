b3f0f500906676d38718b6f075c8582e779a6bd0
simoncarlson <cimon.carlson@gmail.com>
2019-11-18 12:21:48 +0100

    Associate parsing functions to token types

--- 01/src/monkey/parser/parser.go
+++ 01/src/monkey/parser/parser.go
@@ -7,11 +7,19 @@ import (
 	"monkey/token"
 )
 
+type (
+	prefixParseFn func() ast.Expression
+	infixParseFn func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
-	l *lexer.Lexer
+	l 			*lexer.Lexer
 	curToken	token.Token
 	peekToken	token.Token
 	errors		[]string
+
+	prefixParseFns 	map[token.TokenType]prefixParseFn
+	infixParseFns 	map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -28,6 +36,14 @@ func New(l *lexer.Lexer) *Parser {
 
 func (p *Parser) Errors() []string {
 	return p.errors
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) nextToken() {
