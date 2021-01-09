1c6ee2e2f3e470d7332977f80fa54cd215a52734
simoncarlson <cimon.carlson@gmail.com>
2021-01-09 15:05:29 +0100

    Evaluate hash literals

--- 01/src/monkey/evaluator/evaluator_test.go
+++ 01/src/monkey/evaluator/evaluator_test.go
@@ -343,7 +343,7 @@ func TestStringConcatenation(t *testing.T) {
 
 func TestBuiltinFunctions(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected interface{}
 	}{
 		{`len("")`, 0},
@@ -406,7 +406,7 @@ func TestArrayLiterals(t *testing.T) {
 
 func TestArrayIndexExpressions(t *testing.T) {
 	tests := []struct {
-		input string
+		input    string
 		expected interface{}
 	}{
 		{
@@ -459,5 +459,46 @@ func TestArrayIndexExpressions(t *testing.T) {
 		} else {
 			testNullObject(t, evaluated)
 		}
+	}
+}
+
+func TestHashLiterals(t *testing.T) {
+	input := `let two = "two";
+    {
+        "one": 10 - 9,
+        two: 1 + 1,
+        "thr" + "ee": 6 / 2,
+        4: 4,
+        true: 5,
+        false: 6,
+    }`
+
+	evaluated := testEval(input)
+	t.Log(evaluated)
+	result, ok := evaluated.(*object.Hash)
+	if !ok {
+		t.Fatalf("Eval didn't return Hash. got=%T (%+v)", evaluated, evaluated)
+	}
+
+	expected := map[object.HashKey]int64{
+		(&object.String{Value: "one"}).HashKey()  : 1,
+		(&object.String{Value: "two"}).HashKey()  : 2,
+		(&object.String{Value: "three"}).HashKey(): 3,
+		(&object.Integer{Value: 4}).HashKey()     : 4,
+		TRUE.HashKey()                            : 5,
+		FALSE.HashKey()                           : 6,
+	}
+
+	if len(result.Pairs) != len(expected) {
+		t.Fatalf("Hash has wrong num of pairs. got=%d", len(result.Pairs))
+	}
+
+	for expectedKey, expectedValue := range expected{
+		pair, ok := result.Pairs[expectedKey]
+		if !ok {
+			t.Errorf("no pair for given key in Pairs")
+		}
+
+		testIntegerObject(t, pair.Value, expectedValue)
 	}
 }
